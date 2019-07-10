f2023a310d256ff9055249cbf390a896d0dff1ad
Meredith Parr <meredith@merediths-mbp.lan>
2019-07-10 07:08:25 -0500

    add sheets storage app

---
+++ sheets-storage-manager/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import ExampleComponent from './components/ExampleComponent';
+import './App.scss';
+
+class App extends Component {
+
+	render() {
+		return (
+      <div className="app">
+          <header className="app__header">
+              <img src={logo} className="app__logo" alt="logo" />
+              <h1 className="app__title">The Saatva Company - Storage Space Manager</h1>
+          </header>
+          <div>
+              <ExampleComponent />
+          </div>
+      </div>
+		);
+	}
+}
+
+export default App;
--- storage-space-manager/src/App.js
+++ storage-space-manager/src/App.js
@@ -3,16 +3,16 @@ import logo from './logo.svg';
 import ExampleComponent from './components/ExampleComponent';
 import axios from 'axios';
 import variables from './config/variables';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
 
 	render() {
 		return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">The Saatva Company - Storage Space Manager</h1>
+            <div className="app">
+                <header className="app__header">
+                    <img src={logo} className="app__logo" alt="logo" />
+                    <h1 className="app__title">The Saatva Company - Storage Space Manager</h1>
                 </header>
                 <div>
                     <ExampleComponent />
